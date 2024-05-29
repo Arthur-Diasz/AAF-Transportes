fe963dab2693e790442744623dafcb4d683ce147
Arthur Dias <117912903+Arthur-Diasz@users.noreply.github.com>
2024-05-29 16:43:06 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,3 +3,43 @@ document.querySelectorAll('.nav-link').forEach(item => {
       document.querySelector('.navbar-collapse').classList.remove('show');
     })
   });
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    const sectionQuemSomos = document.getElementById('quem-somos');
+    observer.observe(sectionQuemSomos);
+    
+    const sectionMVPO = document.getElementById('mvpo');
+    observer.observe(sectionMVPO);
+
+    const sectionDepoimento = document.getElementById('depoimento');
+    observer.observe(sectionDepoimento);
+
+    const sectionServicos = document.getElementById('servicos');
+    observer.observe(sectionServicos);
+
+    const sectionCertificados = document.getElementById('certificados');
+    observer.observe(sectionCertificados);
+
+    const sectionFrota = document.getElementById('frota');
+    observer.observe(sectionFrota);
+
+    const sectionEquipe = document.getElementById('equipe');
+    observer.observe(sectionEquipe);
+
+    const sectionFooter = document.getElementById('footer');
+    observer.observe(sectionFooter);
+});
+
+
